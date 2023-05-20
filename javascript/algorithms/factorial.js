51016471d2862d6661b1215c5a331267f19c4e84
Paul Aderoju <paul.adeboye@yahoo.com>
2023-05-20 01:58:35 +0100

    Factorial JS

---
+++ javascript/algorithms/factorial.js
@@ -0,0 +1,24 @@
+// Given a number 'n', find n!
+
+// Function declaration
+function factotial(n) {
+
+    // Since 0! and 1! are equal to 1, define a variable as 1
+    let result = 1;
+
+    // Loop through 2 and n, and keep multiplying the result by the iterand
+    for (i = 2; i <= n; i++) {
+
+        result = result * i;
+    }
+
+    // Return result
+    return result;
+};
+
+console.log(factotial(0));
+console.log(factotial(1));
+console.log(factotial(4))
+console.log(factotial(5));
+
+// Big O notation = O(n) - Linear 
